ab33872b4935fcfd55706a17296361d406563008
Arron M <marshall.arron@gmail.com>
2019-06-12 09:14:57 -0700

    Refactor auth route to use async await

--- src/server/routers/auth.js
+++ src/server/routers/auth.js
@@ -19,50 +19,48 @@ router.post('/register', validateBody({
     type: 'string',
     require: true,
   },
-}), (req, res) => {
-  let user = req.body;
+}), async (req, res) => {
+  try {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 8);
 
-  const hash = bcrypt.hashSync(user.password, 8);
-
-  user.password = hash;
-
-  db.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(errorRef(error));
+    const saved = await db.add({
+      ...user,
+      password: hash,
     });
-});
-
-router.post('/login', (req, res) => {
-  let { username, password } = req.body;
-
-  db.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
-        res.status(200).json({
-          message: `Welcome ${user.username}!, have a cookie`,
-          token,
-        });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(errorRef(error));
-    });
-});
-
-router.delete('/logout', (req, res) => {
-  if (req.session && req.session.username) {
-    req.session.destroy();
+    res.status(201).json(saved);
+  } catch (error) {
+    res.status(500).json(errorRef(error))
   }
-  res.json({
-    message: 'You have been logged out.',
-  });
 });
+
+router.post('/login', async(req, res) => {
+  try {
+    let { username, password } = req.body;
+    const user = await db.findBy({ username }).first();
+
+    if (!(user && bcrypt.compareSync(password, user.password))) return res.status(401).json({
+      message: 'Invalid Credentials',
+    });
+
+    const token = generateToken(user);
+
+    res.json({
+      message: `Welcome ${user.username}`,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json(errorRef(error));
+  }
+});
+
+// router.delete('/logout', (req, res) => {
+//   if (req.session && req.session.username) {
+//     req.session.destroy();
+//   }
+//   res.json({
+//     message: 'You have been logged out.',
+//   });
+// });
 
 module.exports = router;
